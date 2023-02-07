0806e892622abf765f03c4613cdca4991694caca
root <root@asterisk1.sbtphilippines.inc>
2023-02-08 03:36:19 +0800

    First Commit

---
+++ config/database.php
@@ -0,0 +1,38 @@
+<?php
+
+class Database{
+
+    //database properties
+
+    private $host = "192.168.70.250";
+    private $dbname = "sbtphcsd";
+    // private $dbname = "restore_data";
+    private $username = "python";
+    private $password = "sbtph@2018";
+
+ 
+    private $db;
+
+    //database method/function
+
+    public function getConnection(){
+
+    		$conn = NULL;
+    	
+    	try{
+    			$conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", "$this->username", "$this->password");
+    			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    	}catch (PDOExeption $e){
+    		echo 'ERROR: ' . $e->getMessage();
+    	}
+
+    	$this->db = $conn;
+    	return $this->db;
+
+    }
+
+
+
+
+}
