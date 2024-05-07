8be66014206cd698b9e44961c9584d211ae0c8e8
Rogmer Bulaclac <rogmer2022.bulaclac@gmail.com>
2024-05-08 01:32:26 +0800

    latest code changes

--- objects/csd.php
+++ objects/csd.php
@@ -1118,7 +1118,7 @@ class Csd {
       //  return;
 	    //delete query
 	    $query = "DELETE FROM `customer_info` WHERE `customer_number`='$this->customer_number' AND `cid`='$this->customer_id' AND  `customer_name`='$this->customer_name' AND `updated_by`='$this->updated_by'";
-      $query = "DELETE FROM `inbound_callstatus` WHERE `WhoAnsweredCall`='$extension'";
+     
        
 	    // prepare query
 	    $stmnt = $this->conn->prepare($query);
