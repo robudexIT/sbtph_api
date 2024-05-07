1805c18043c8c6776c4d674346e8199001fcf1d1
Rogmer Bulaclac <rogmer2022.bulaclac@gmail.com>
2024-05-08 01:20:17 +0800

    adding delete api

--- objects/csd.php
+++ objects/csd.php
@@ -1113,7 +1113,7 @@ class Csd {
       $this->updated_by = htmlspecialchars(strip_tags($this->updated_by));
 
 
-      return;
+      
  
 	    //delete query
 	    $query = "DELETE FROM `customer_info_05012024` WHERE `customer_number`='$this->customer_number' AND `cid`='$this->customer_id' AND  `customer_name`='$this->customer_name' AND `updated_by`='$this->updated_by'";
