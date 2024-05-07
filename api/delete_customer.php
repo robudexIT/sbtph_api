e4572d0e5b45e72aef652a8181a8551be033b31e
Rogmer Bulaclac <rogmer2022.bulaclac@gmail.com>
2024-05-07 22:27:48 +0800

    add delete api

---
+++ api/delete_customer.php
@@ -0,0 +1,26 @@
+<?php
+//required headers
+header("Access-Control-Allow-Origin: * ");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: DELETE");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+// // database connection will be here...
+
+//include database and object files
+include_once '../config/database.php';
+include_once '../objects/csd.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$csd = new Csd($db);
+//  // get posted data
+  $data = json_decode(file_get_contents("php://input"));
+
+
+  $customer_number = $data->customer_number;
+  $customer_id = $data->customer_id;
+  $customer_name = $data->customer;
+  
+  $csd->deleteCustomer($customer_number,$customer_id, $customer_name);
