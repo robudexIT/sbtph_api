f4efca4395cf531a8c8ec69e23fe38709e2bfa96
Rogmer Bulaclac <rogmer2022.bulaclac@gmail.com>
2024-05-08 01:25:33 +0800

    adding delete api

--- objects/csd.php
+++ objects/csd.php
@@ -1112,12 +1112,13 @@ class Csd {
       $this->customer_name = htmlspecialchars(strip_tags($this->customer_name));
       $this->updated_by = htmlspecialchars(strip_tags($this->updated_by));
 
-
+     
+      echo $this->customer_number . $this->customer_number . $this->customer_name .  $this->updated_by;
       
- 
+       return;
 	    //delete query
 	    $query = "DELETE FROM `customer_info_05012024` WHERE `customer_number`='$this->customer_number' AND `cid`='$this->customer_id' AND  `customer_name`='$this->customer_name' AND `updated_by`='$this->updated_by'";
-      
+      $query = "DELETE FROM `inbound_callstatus` WHERE `WhoAnsweredCall`='$extension'";
        
 	    // prepare query
 	    $stmnt = $this->conn->prepare($query);
